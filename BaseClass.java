2377cc77e6e8c7cf2337b57c72757a23c74e628f
Kanchana1506 <68426134+Kanchana1506@users.noreply.github.com>
2020-08-26 01:02:31 +0530

    Add files via upload

---
+++ BaseClass.java
@@ -0,0 +1,53 @@
+package week5.day2.classoom;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+public class BaseClass {
+
+	public ChromeDriver driver;
+	@Parameters({"username","password"}) 	
+	
+		@BeforeMethod
+		public void login(String uname, String pname)
+		{
+			  System.setProperty("webdriver.chrome.driver","./drivers\\chromedriver.exe");
+				driver = new ChromeDriver();
+				
+				//1. Launch URL "http://leaftaps.com/opentaps/control/login"
+				driver.get("http://leaftaps.com/opentaps/control/login");
+				driver.manage().window().maximize();
+				
+				
+				//2. Enter UserName and Password Using Id Locator
+				WebElement usrname = driver.findElementById("username");
+				usrname.sendKeys(uname);
+
+				WebElement pwd = driver.findElementById("password");
+				pwd.sendKeys(pname);
+				
+				//3. Click on Login Button using Class Locator
+				WebElement submit = driver.findElementByClassName("decorativeSubmit");
+				submit.click();
+				
+				// 4. Click on CRM/SFA Link1
+				WebElement crmlink = driver.findElementByLinkText("CRM/SFA");
+				crmlink.click();
+				
+				//5. Click on Leads Button
+				WebElement leads = driver.findElementByLinkText("Leads");
+				leads.click();
+	
+		}
+
+		@AfterMethod
+		public void close()
+		{
+			driver.close();
+			
+		}
+	}
+
